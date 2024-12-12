62fe2fd70b5aa993af6093f840c054e7c6dbf614
https://github.com/Zuber-shaikh <shaikhzuber2000@gmail.com>
2024-12-12 19:44:59 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 function App() {
   const [numAllowed, setNumAllowed] = useState(false);
@@ -6,7 +6,11 @@ function App() {
   const [length, setLength] = useState(8);
   const [password, setPassword] = useState("");
 
-  function createPassword() {
+  // useRef hook to have link between copy button and inputText
+  // whenever we click on copy button it would highlight the text
+  const passwordRef = useRef(null);
+
+  const createPassword = useCallback(() => {
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     let newPassword = "";
 
@@ -22,48 +26,82 @@ function App() {
       newPassword += str.charAt(random);
     }
     setPassword(newPassword);
-  }
+  }, [length, numAllowed, specialCharAllowed, setPassword]);
+
+  const copyPasswordToClipboar = useCallback(() => {
+    passwordRef.current?.select(); // will highlight the complete text when we click on copy button
+    passwordRef.current?.setSelectionRange(0, 4);
+    window.navigator.clipboard.writeText(password);
+  }, [password])
 
   useEffect(() => {
     createPassword();
-  }, [length, numAllowed, specialCharAllowed, setPassword])
+  }, [length, numAllowed, specialCharAllowed, createPassword]);
 
   return (
-    <div className="w-full h-screen bg-black">
-      <div>
-        <div className="flex justify-center">
-          <h1 className="bg-white px-5 py-5 my-10 rounded-xl">
-            Password Generator
-          </h1>
-        </div>
+    // <div className="flex justify-center ">
+    <div className="self-center w-full max-w-lg mx-auto my-8 px-4 py-3 bg-white text-orange-500 rounded-lg shadow-md">
+      <h1 className="text-slate-700 text-center text-2xl my-3">
+        Password Generator
+      </h1>
 
-        <div className="flex justify-center bg-slate-400">
-          <div className=" my-5 px-5 py-5 ">
-            <input
-              value={password}
-              readOnly
-            ></input>
-          </div>
-          <div>  
-            <input
+      <div className="flex shadow rounded-lg overflow-hidden mb-4">
+        <input
+          type="text"
+          value={password}
+          className="outline-none w-full py-1 px-3"
+          placeholder="Password"
+          readOnly
+          ref={passwordRef}
+        />
+
+        <button
+          onClick={copyPasswordToClipboar}
+          className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0"
+        >
+          copy
+        </button>
+      </div>
+
+      <div className="flex text-sm gap-x-2">
+        <div className="flex items-center gap-x-1">
+          <input
+            className="cursor-pointer"
+            type="range"
+            min={8}
+            max={50}
+            value={length}
+            onChange={(event) => {
+              setLength(event.target.value);
+            }}
+          />
+          <label>Length: {length}</label>
+        </div>
+        <div className="flex items-center gap-x-1">
+          <input
             type="checkbox"
             defaultChecked={numAllowed}
+            id="numberInput"
             onChange={() => {
               setNumAllowed((prev) => !prev);
             }}
-            >
-
-            </input>
-            <input 
+          />
+          <label>Numbers</label>
+        </div>
+        <div className="flex items-center gap-x-1">
+          <input
             type="checkbox"
+            defaultChecked={specialCharAllowed}
+            id="specialChar"
             onChange={() => {
               setSpecialCharAllowed((prev) => !prev);
             }}
-            ></input>
-          </div>
+          />
+          <label>Special Characters</label>
         </div>
       </div>
     </div>
+    // </div>
   );
 }
 
